016b6066b49adf9612e4dbbec776ec1bd3f8946e
ELEKS-SOFTWARE\iryna.bokalo <iraklepak@gmail.com>
2017-04-14 00:10:55 +0300

    Some changes to repository pattern.

---
+++ Domain/Abstract/IExcursionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Entities;
+
+namespace Domain.Abstract
+{
+    public interface IExcursionRepository:IDisposable
+    {
+        IEnumerable<Excursion> GetExcursions();
+        Excursion GetExcursionById(int excursionId);
+        void InsertExcursion(Excursion excursion);
+        void DeleteExcursion(int excursionId);
+        void UpdateExcursion(Excursion excursion);
+        void Save();
+    }
+}
--- Domain/Concrete/Repository.cs
+++ Domain/Concrete/Repository.cs
@@ -4,11 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Domain.Abstract;
+using Domain.Entities;
 
 namespace Domain.Concrete
 {
-    public class Repository<TEntity> : IRepository where TEntity : class
+    public class ExcursionRepository : IExcursionRepository
     {
 
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Excursion> GetExcursions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Excursion GetExcursionById(int excursionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertExcursion(Excursion excursion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteExcursion(int excursionId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateExcursion(Excursion excursion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
