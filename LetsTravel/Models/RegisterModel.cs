d134511c8ce32747aacad3cd30a35df2204755c4
Ulyana Zhovtanetska <ulyana.zhovtanetska@gmail.com>
2017-04-20 02:01:15 +0300

    Register view added

--- LetsTravel/Models/RegisterModel.cs
+++ LetsTravel/Models/RegisterModel.cs
@@ -9,10 +9,12 @@ namespace LetsTravel.Models
     public class RegisterModel
     {
         [Required]
-        public string Name { get; set; }
+        public string Nickname { get; set; }
         [Required]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
+        [Required]
+        public string Role { get; set; }
     }
 }
