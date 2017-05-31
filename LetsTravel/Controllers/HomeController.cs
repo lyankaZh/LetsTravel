63237a5f8d7ee0274af3c1d061d013a8eccaac26
Ulyana Zhovtanetska <ulyana.zhovtanetska@gmail.com>
2017-05-31 11:45:15 +0300

    First try to add localization.

--- LetsTravel/Controllers/HomeController.cs
+++ LetsTravel/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LetsTravel.Helpers;
 
 namespace LetsTravel.Controllers
 {
@@ -60,6 +61,22 @@ namespace LetsTravel.Controllers
                 ViewBag.MenuItemName = "Explore it";
                 ViewBag.LogInOrOutText = "Log in";
             }
+        }
+
+        public ActionResult ChangeCurrentCulture(int id)
+        {
+            //  
+            // Change the current culture for this user.  
+            //  
+            CultureHelper.CurrentCulture = id;
+            //  
+            // Cache the new current culture into the user HTTP session.   
+            //  
+            Session["CurrentCulture"] = id;
+            //  
+            // Redirect to the same page from where the request was made!   
+            //  
+            return Redirect(Request.UrlReferrer.ToString());
         }
 
     }
---
+++ LetsTravel/Helpers/CultureHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.SessionState;
+
+namespace LetsTravel.Helpers
+{
+    public class CultureHelper
+    {
+        protected HttpSessionState session;
+
+        public CultureHelper(HttpSessionState httpSessionState)
+        {
+            session = httpSessionState;
+        }
+
+        public static int CurrentCulture
+        {
+            get
+            {
+                if (Thread.CurrentThread.CurrentUICulture.Name == "en-GB")
+                {
+                    return 0;
+                }
+                else if (Thread.CurrentThread.CurrentUICulture.Name == "uk-UA")
+                {
+                    return 1;
+                }
+
+                else
+                {
+                    return 1;
+                }
+            }
+            set
+            {
+
+                if (value == 0)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB");
+                }
+                else if (value == 1)
+                {
+                    Thread.CurrentThread.CurrentUICulture = new CultureInfo("uk-UA");
+                }
+               
+                else
+                {
+                    Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+                }
+
+                Thread.CurrentThread.CurrentCulture = Thread.CurrentThread.CurrentUICulture;
+
+            }
+        }
+    }
+}
